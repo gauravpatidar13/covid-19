8a03f24c54d157d5724f0a89b283901fc7b2143f
Gaurav patidar <gp26101998@gmail.com>
2021-02-01 18:11:56 +0530

    first usteer dashboard page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { GravidaComponent } from './gravida/gravida.component';
-import { JustoComponent } from './justo/justo.component';
-import { PurusComponent } from './purus/purus.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    GravidaComponent,
-    JustoComponent,
-    PurusComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PerfectScrollbarModule
   ],
-  providers: [],
+  providers: [{
+    provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
